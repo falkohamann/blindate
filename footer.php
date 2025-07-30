2ce16f752505b9611f01672995cfb7eacad28d09
Falko Hamann <falko.hamann@sap.com>
2025-07-30 21:48:04 +0200

    certified

--- footer.php
+++ footer.php
@@ -5,10 +5,15 @@
                 <h3>Blindate</h3>
                 <p>Live seit mehr als 20 Jahren!</p>
             </div>
-            <div class="footer-section" style="flex: 1; display: flex; justify-content: center; align-items: center;">
+            <div class="footer-section" style="flex: 1; display: flex; flex-direction: column; justify-content: center; align-items: center;">
                 <div style="text-align: center;">
                     <a href="https://denkinstanz.de" target="_blank" rel="noopener">
                         <img src="images/website/denkinstanz.webp" alt="Denkinstanz Logo" style="max-width: 100%; height: auto;">
+                    </a>
+                </div>
+                <div style="margin-top: 1rem;">
+                    <a href="https://jigsaw.w3.org/css-validator/check/referer">
+                        <img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss" alt="CSS ist valide!" />
                     </a>
                 </div>
             </div>
