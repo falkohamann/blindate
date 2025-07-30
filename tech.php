bc579d842782898a0915327e95d599b74a1fe071
Falko Hamann <falko.hamann@sap.com>
2025-07-30 21:29:46 +0200

    added Referenzen

--- tech.php
+++ tech.php
@@ -6,16 +6,32 @@
 <section class="about" id="tech">
     <div class="about-container">
         <div>
-            <h2 style="color: var(--primary);">Veranstalter / Technik</h2>
+            <h2 class="primary-color">Veranstalter / Technik</h2>
             <p>
                 Hier finden Veranstalter und Presse relevante Informationen und technische Details.
             </p>
-            <ul style="margin-top: 2em;">
+            <ul class="pdf-links">
+            <h3 class="referenzen-title">Referenzen</h3>
+            <ul class="referenzen-list">
+                <li>Stadtfest Dippoldiswalde</li>
+                <li>Bergstadt Freiberg</li>
+                <li>Da Capon Leipzig</li>
+                <li>Brauereifest Apolda</li>
+                <li>Bärenzwinger Dresden</li>
+                <li>Theater der Jungen Generation Dresden</li>
+                <li>Schloß Wesenstein</li>
+                <li>Messe Hanover</li>
+                <li>Schloß Wermsdorf</li>
+                <li>Dorf- und Stadtfeste</li>
+                <li>Firmen-Events</li>
+                <li>Hochzeiten, Polterabende, Geburtstage, Provatpartys aller Art</li>
+                <li>und en Haufen anderen Kram</li>
+            </ul>
                 <li>
-                    <a href="2025_Pressetexte.pdf" download style="font-weight: bold; color: var(--primary);">Pressetext herunterladen (PDF)</a>
+                    <a href="2025_Pressetexte.pdf" download class="pdf-link">Pressetext herunterladen (PDF)</a>
                 </li>
                 <li>
-                    <a href="2025_TecRider.pdf" download style="font-weight: bold; color: var(--primary);">TechRider herunterladen (PDF)</a>
+                    <a href="2025_TecRider.pdf" download class="pdf-link">TechRider herunterladen (PDF)</a>
                 </li>
             </ul>
             <!-- Weitere Inhalte können hier ergänzt werden -->
