f4db07350cace7fdba55e352ec614232b00c37df
Falko Hamann <falko.hamann@sap.com>
2025-07-30 21:10:00 +0200

    favicon added

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blindate</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="/styles.css">
+    <link rel="icon" type="image/png" href="/images/website/favicon.png">
 </head>
 <body>
     <nav class="nav">
@@ -22,11 +23,11 @@
         </button>
     </div>
     <div class="nav-links nav-links-right">
-        <a href="index.php" class="nav-link">Home</a>
-        <a href="#live-music" class="nav-link">Live</a>
-        <a href="#about" class="nav-link">Band</a>
-        <a href="#media" class="nav-link">Media</a>
-        <a href="tech.php" class="nav-link">Veranstalter</a>
-        <a href="#contact" class="nav-link">Kontakt</a>
+        <a href="/index.php" class="nav-link">Home</a>
+        <a href="/index.php#live-music" class="nav-link">Live</a>
+        <a href="/index.php#about" class="nav-link">Band</a>
+        <a href="/index.php#media" class="nav-link">Media</a>
+        <a href="/tech.php" class="nav-link">Veranstalter</a>
+        <a href="/index.php#contact" class="nav-link">Kontakt</a>
     </div>
 </nav>
